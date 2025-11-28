fa9fb41457d98b314aa897ab6ac72491365e620f
JustMili <rodx160@gmail.com>
2025-11-28 18:30:55 +0100

    Holy shit it works

--- common/src/main/java/net/justmili/AlwaysShield.java
+++ common/src/main/java/net/justmili/AlwaysShield.java
@@ -2,7 +2,5 @@ package net.justmili;
 
 public final class AlwaysShield {
     public static final String MOD_ID = "alwaysshield";
-    public static void init() {
-
-    }
+    public static void init() {}
 }
--- common/src/main/java/net/justmili/mixin/SwordBlocking.java
+++ common/src/main/java/net/justmili/mixin/SwordBlocking.java
@@ -3,12 +3,11 @@ package net.justmili.mixin;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.ShieldItem;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.UseAnim;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,21 +20,22 @@ public class SwordBlocking {
     private void use(Level world, Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResultHolder<ItemStack>> cir) {
         ItemStack stack = player.getItemInHand(hand);
         if (stack.getItem() instanceof SwordItem) {
-            ItemStack off = player.getOffhandItem();
-            if (!off.isEmpty() && off.getItem() instanceof ShieldItem) {
-                player.startUsingItem(hand);
-                cir.setReturnValue(InteractionResultHolder.consume(stack));
-            }
+            player.startUsingItem(hand);
+            cir.setReturnValue(InteractionResultHolder.consume(stack));
         }
     }
 
     @Inject(method = "getUseAnimation", at = @At("HEAD"), cancellable = true)
     private void getUseAnimation(ItemStack stack, CallbackInfoReturnable<UseAnim> cir) {
-        if (stack.getItem() instanceof SwordItem) cir.setReturnValue(UseAnim.BLOCK);
+        if (stack.getItem() instanceof SwordItem) {
+            cir.setReturnValue(UseAnim.BLOCK);
+        }
     }
 
     @Inject(method = "getUseDuration", at = @At("HEAD"), cancellable = true)
     private void getUseDuration(ItemStack stack, LivingEntity livingEntity, CallbackInfoReturnable<Integer> cir) {
-        if (stack.getItem() instanceof SwordItem) cir.setReturnValue(72000);
+        if (stack.getItem() instanceof SwordItem) {
+            cir.setReturnValue(72000);
+        }
     }
 }

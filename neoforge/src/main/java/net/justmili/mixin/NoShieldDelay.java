fb0da92780f63eea1f2a08fdcee794aebd7d74fa
JustMili <rodx160@gmail.com>
2025-12-11 21:48:07 +0100

    1.21.5

--- neoforge/src/main/java/net/justmili/mixin/NoShieldDelay.java
+++ neoforge/src/main/java/net/justmili/mixin/NoShieldDelay.java
@@ -1,14 +1,33 @@
 package net.justmili.mixin;
 
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemUseAnimation;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import org.jetbrains.annotations.Nullable;
 
 @Mixin(LivingEntity.class)
 public class NoShieldDelay {
-    @ModifyConstant(method = "getItemBlockingWith", constant = @Constant(intValue = 5))
-    private int removeShieldDelay(int constant) {
-        return 0;
+    @Shadow @Nullable protected ItemStack useItem;
+
+    @Inject(method = "getItemBlockingWith", at = @At("HEAD"), cancellable = true)
+    private void removeShieldDelay(CallbackInfoReturnable<ItemStack> cir) {
+        LivingEntity self = (LivingEntity) (Object) this;
+
+        if (!self.isUsingItem()) { return; }
+        if (this.useItem == null || this.useItem.isEmpty()) { return; }
+
+        Item item = this.useItem.getItem();
+        if (item.getUseAnimation(this.useItem) != ItemUseAnimation.BLOCK) {
+            return;
+        }
+
+        cir.setReturnValue(this.useItem);
     }
 }

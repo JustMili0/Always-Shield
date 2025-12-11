06cad15683c8e2d9c9c26d36d13126a6e97dddbe
JustMili <rodx160@gmail.com>
2025-12-11 19:02:36 +0100

    Switched to Yarn, made it work on Fabric, needs work on NeoForge

--- fabric/src/main/java/net/justmili/mixin/NoShieldDelay.java
+++ fabric/src/main/java/net/justmili/mixin/NoShieldDelay.java
@@ -1,16 +1,14 @@
 package net.justmili.mixin;
 
-import net.minecraft.world.item.component.Consumable;
+import net.minecraft.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(Consumable.class)
+@Mixin(LivingEntity.class)
 public class NoShieldDelay {
-    @Inject(method = "consumeTicks", at = @At("HEAD"), cancellable = true)
-    private void removeShieldDelay(CallbackInfoReturnable<Integer> cir) {
-        //TODO: find a way to limit this to only ShieldItem
-        cir.setReturnValue(0);
+    @ModifyConstant(method = { "getBlockingItem" }, constant = { @Constant(intValue = 5) })
+    private int removeShieldDelay(int constant) {
+        return 0;
     }
 }

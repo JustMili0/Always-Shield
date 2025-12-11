48bb4c17e2624eb725439212b908a17553be7672
JustMili <rodx160@gmail.com>
2025-12-11 19:51:52 +0100

    NeoForge works

--- neoforge/src/main/java/net/justmili/mixin/NoShieldDelay.java
+++ neoforge/src/main/java/net/justmili/mixin/NoShieldDelay.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(value = LivingEntity.class, remap = false)
+@Mixin(LivingEntity.class)
 public class NoShieldDelay {
     @ModifyConstant(method = "getItemBlockingWith", constant = @Constant(intValue = 5))
     private int removeShieldDelay(int constant) {

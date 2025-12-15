0fa70f516d9781b2a85bad7119af54f729594792
JustMili <rodx160@gmail.com>
2025-12-15 19:53:35 +0100

    [EXPERIMENTAL] Forge!

---
+++ forge/src/main/java/net/justmili/forge/AlwaysShieldForge.java
@@ -0,0 +1,12 @@
+package net.justmili.forge;
+
+import net.minecraftforge.fml.common.Mod;
+
+import net.justmili.AlwaysShield;
+
+@Mod(AlwaysShield.MOD_ID)
+public final class AlwaysShieldForge {
+    public AlwaysShieldForge() {
+        AlwaysShield.init();
+    }
+}

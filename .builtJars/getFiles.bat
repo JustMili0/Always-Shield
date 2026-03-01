16ce29fb55fa36e5313e0ad2ab1767a51cfd46a7
JustMili <rodx160@gmail.com>
2026-03-01 10:26:19 +0100

    1.2.1 fix on forge

--- .builtJars/getFiles.bat
+++ .builtJars/getFiles.bat
@@ -1,6 +1,5 @@
 @echo off
 if not exist .\fabric\ mkdir fabric
-if not exist .\quilt\ mkdir quilt
 if not exist .\forge\ mkdir forge
 if not exist .\neoforge\ mkdir neoforge
 
@@ -10,13 +9,10 @@ move ..\quilt\build\libs\*.jar . >nul
 move ..\forge\build\libs\*.jar . >nul
 move ..\neoforge\build\libs\*.jar . >nul
 
-timeout /t 3 /nobreak >nul
 echo Deleting files...
 del /q *dev-shadow.jar >nul
 
-timeout /t 1 /nobreak >nul
 echo Sorting files...
 move .\*Fabric.jar .\fabric\
-move .\*Quilt.jar .\quilt\
 move .\*NeoForge.jar .\neoforge\
 move .\*Forge.jar .\forge\
